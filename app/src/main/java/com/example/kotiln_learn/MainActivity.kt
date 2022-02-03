36d5806e825191d55f7d5e5ad24c9e4df26e69ae
이진영 <roswkzm@naver.com>
2022-02-03 20:30:09 +0900

    프로젝트 초기화

--- app/src/main/java/com/example/kotiln_learn/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_learn/MainActivity.kt
@@ -21,29 +21,11 @@ class MainActivity : AppCompatActivity() {
         // getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의 인스턴스를 활용하여 생선된 뷰를 액티비티에 표시.
         setContentView(binding.root)
 
-        loadData()
+
         }
 
     override fun onDestroy() {
         mBinding = null
         super.onDestroy()
-
-        savaData()
-    }
-
-    private fun savaData() {
-        // editText 값을 저장한다.
-        val pref = getSharedPreferences("pref", 0)
-        val edit = pref.edit()      // 수정 모드
-        edit.putString("name", binding.etHello.text.toString())      // 1번째 인자는 키값을, 2번째 인자에는 실제 담아둘 값
-        edit.apply()        // 저장 완료
-    }
-
-    private fun loadData() {
-        // editText 값을 불러와서 다시 넣어준다.
-        val pref = getSharedPreferences("pref", 0)
-
-        // 첫번째 인자에서는 저장할 당시의 키 값을 입력, 2번째는 키 값에 데이터가 졵하지 않을 경우 대체 값을 저장한다.
-        binding.etHello.setText(pref.getString("name", ""))
     }
 }
