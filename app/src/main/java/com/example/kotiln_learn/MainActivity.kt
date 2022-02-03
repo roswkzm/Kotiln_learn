065d8856575bfcf8ddc4e25e4fd144aa10011d1f
이진영 <roswkzm@naver.com>
2022-02-03 19:53:56 +0900

    프로젝트 초기화

--- app/src/main/java/com/example/kotiln_learn/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_learn/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.core.view.GravityCompat
 import com.example.kotiln_learn.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     private var mBinding: ActivityMainBinding? = null   // 전역 변수로 바인딩 객체 선언
     private val binding get() = mBinding!!      // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
@@ -21,33 +21,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의 인스턴스를 활용하여 생선된 뷰를 액티비티에 표시.
         setContentView(binding.root)
 
-        binding.btnNavi.setOnClickListener {
-            binding.layoutDrawer.openDrawer(GravityCompat.START)    // START = left    END = right
-        }
-        binding.naviView.setNavigationItemSelectedListener(this)    // 네비게이션 메뉴 아이템에 클릭 속성을 부여
 
         }
 
     override fun onDestroy() {
         mBinding = null
         super.onDestroy()
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {    // 네이게이션 메뉴 아이템 클릭 시 수행하는 부분
-        when(item.itemId){
-            R.id.access -> Toast.makeText(applicationContext, "접근성", Toast.LENGTH_SHORT).show()
-            R.id.email -> Toast.makeText(applicationContext, "이메일", Toast.LENGTH_SHORT).show()
-            R.id.message -> Toast.makeText(applicationContext, "메시지", Toast.LENGTH_SHORT).show()
-        }
-        binding.layoutDrawer.closeDrawers()     // 버튼 클릭 후 네비게이션 뷰 닫기
-        return false
-    }
-
-    override fun onBackPressed() {
-        if(binding.layoutDrawer.isDrawerOpen(GravityCompat.START)) {
-            binding.layoutDrawer.closeDrawers()     // 뒤로 가기 버튼 누를시 네비게이션 뷰 닫기
-        }else {
-            super.onBackPressed()
-        }
     }
 }
