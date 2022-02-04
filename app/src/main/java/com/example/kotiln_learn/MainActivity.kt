36da2e0479f81905ab3c4f7d5f4c482ab0a12f98
이진영 <roswkzm@naver.com>
2022-02-04 13:49:36 +0900

    카메라 이용하여 저장하기

--- app/src/main/java/com/example/kotiln_learn/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_learn/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.kotiln_learn
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.core.view.GravityCompat
 import com.example.kotiln_learn.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1   // 카메라 사진 촬영 요청 코드
+    lateinit var curPhotoPath: String   // 문자열 형태의 사진 경로 값(초기값을 null로 시작하고 싶을때)
 
     private var mBinding: ActivityMainBinding? = null   // 전역 변수로 바인딩 객체 선언
     private val binding get() = mBinding!!      // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
@@ -23,8 +42,116 @@ class MainActivity : AppCompatActivity() {
         // getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의 인스턴스를 활용하여 생선된 뷰를 액티비티에 표시.
         setContentView(binding.root)
 
+        
+        setPermission() // 최초의 권한을 허용하는 메서드
+
+        binding.btnCamera.setOnClickListener {
+            takeCapture()       // 기본 카메라 앱을 실행하여 사진 촬영
 
         }
+
+        }
+
+    /**
+     * 카메라 촬영
+     */
+    private fun takeCapture() {
+        // 기본 카메라 앱 실행
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                }catch (ex: IOException){
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.kotiln_learn.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    /**
+     * 이미지 파일 생성
+     */
+    private fun createImageFile(): File {
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timestamp}",".jpg", storageDir)
+            .apply { curPhotoPath = absolutePath }
+    }
+
+    /**
+     * 테드 퍼미션 설정
+     */
+    private fun setPermission() {
+        val permission = object : PermissionListener{
+            override fun onPermissionGranted() {        // 설정해놓은 위험 권한들이 허용 되었을 경우
+                Toast.makeText(this@MainActivity, "권한이 허용 되었습니다.",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {      // 설정해놓은 위험권한 들 중 거부를 한 경우 이곳을 수
+                Toast.makeText(this@MainActivity, "권한이 거 되었습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        TedPermission.with(this)
+            .setPermissionListener(permission)
+            .setRationaleMessage("카메라 앱 사용하시려면 권한을 허용해주세을요")
+            .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> 권한 항목에서 허용해주세요")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+            .check()
+    }
+
+    /**
+     * StartActivityForResult를 통해서 기본 카메라 앱으로 부터 받아온 사진 결과값
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // 이미지를 성공적으로 가져왔다면
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val bitmap: Bitmap
+            val file = File(curPhotoPath)
+            if (Build.VERSION.SDK_INT < 28) {     // 안드로이드 9.0 버전보다 낮을 경우
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
+                binding.ivProfile.setImageBitmap(bitmap)
+            } else {  // 안드로이드 9.0보다 높을 경우
+                val decode = ImageDecoder.createSource(
+                    this.contentResolver,
+                    Uri.fromFile(file)
+                )
+                bitmap = ImageDecoder.decodeBitmap(decode)
+                binding.ivProfile.setImageBitmap(bitmap)
+            }
+            savePhoto(bitmap)
+        }
+    }
+
+    /**
+     * 갤러리에 저장
+     */
+    private fun savePhoto(bitmap: Bitmap) {
+        val absolutePath = "/storage/emulated/0/"
+        val folderPath = "$absolutePath/pictures/"   // 사진폴더로 저장하기 위한 경로 선언
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val fileName = "${timestamp}.jpeg"
+        val folder = File(folderPath)
+        if(!folder.isDirectory){    // 현재 해당 경로에 폴더가 존재 하지 않는다면
+            folder.mkdirs() // 해당 경로에 폴더 새로 만들기
+        }
+
+        // 실제적인 저장처리
+        val out = FileOutputStream(folderPath + fileName)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+        Toast.makeText(this, "사진이 앨범에 저장되었습니다.", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onDestroy() {
         mBinding = null
