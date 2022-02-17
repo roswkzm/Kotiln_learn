dd4887441a7c9bb82fc3afa806c12a44c8855064
이진영 <roswkzm@naver.com>
2022-02-17 19:01:03 +0900

    RecyclerView Viewpager2 -> tabLayout 추가

--- app/src/main/java/com/example/kotiln_learn/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_learn/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotiln_learn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +22,12 @@ class MainActivity : AppCompatActivity() {
             R.drawable.android,
         )
 
+
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab ${position+1}"
+        }.attach()
     }
 }
