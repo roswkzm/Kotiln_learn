d969c15fc355d8f0d55a049ba85f8f104895a844
이진영 <roswkzm@naver.com>
2022-03-21 20:22:27 +0900

    Coroutine과 Retrofit2로 api 가져오기

---
+++ app/src/main/java/com/example/kotiln_learn/ApiRequest.kt
@@ -0,0 +1,15 @@
+package com.example.kotiln_learn
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+interface ApiRequest {
+    companion object{
+        const val BASE_URL = "https://api.corona-19.kr/"
+        const val Token = "WNr54nBbo3w9OCRlpayGhKqmHA1DzLejZ"
+    }
+
+    @GET("/korea/country/new/?serviceKey=${Token}")
+    fun getApi() : Call<ApiData>
+}
--- app/src/main/java/com/example/kotiln_learn/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_learn/MainActivity.kt
@@ -3,16 +3,39 @@ package com.example.kotiln_learn
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.viewpager2.widget.ViewPager2
+import com.example.kotiln_learn.ApiRequest.Companion.BASE_URL
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.awaitResponse
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiRequest::class.java)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            val response : Response<ApiData> = api.getApi().awaitResponse()
+
+            withContext(Dispatchers.Main){
+                Log.d("ㅎㅇㅎㅇ1", response.headers().toString())
+                Log.d("ㅎㅇㅎㅇ1", response.body()?.korea.toString())
+            }
+        }
 
     }
 }
